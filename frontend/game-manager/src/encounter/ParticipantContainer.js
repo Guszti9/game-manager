0853afbe38a8fa76177985adc0b69b4b11d9aaa1
Guszti9 <tusi0531@gmail.com>
2022-07-28 17:53:07 +0200

    add modal

--- frontend/game-manager/src/encounter/ParticipantContainer.js
+++ frontend/game-manager/src/encounter/ParticipantContainer.js
@@ -1,12 +1,34 @@
 import {useState, useEffect} from "react";
 import ParticipantCard from "./ParticipantCard";
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
 
 function ParticipantContainer(props) {
     const [participants, setParticipants] = useState(null)
+    const [modalIsOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         setParticipants(props.participants)
     }, [props]);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
 
     return (
         <div>
@@ -16,9 +38,21 @@ function ParticipantContainer(props) {
                 )
             })}
             <div className="add-participant-container">
-                <button className="add-participant-button">
+                <button className="add-participant-button"
+                        onClick={openModal}>
                     Add Participant
                 </button>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    <h2>Add Participant</h2>
+                    <button onClick={closeModal}>close</button>
+                    <form>
+                    </form>
+                </Modal>
             </div>
         </div>
     );
